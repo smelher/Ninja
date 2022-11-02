95dbca3bc99c118c496aa40ba67d7eef74aa062a
smelher <79649846+smelher@users.noreply.github.com>
2022-11-02 12:07:57 -0400

    Added incomplete documentation steps

--- ninjas_elif_statement.py
+++ ninjas_elif_statement.py
@@ -1,8 +1,7 @@
-
-Ninjas = 5
-if Ninjas <= 100:
+ninjas = 5
+if ninjas <= 100:
     print('''That's too many!''')
-elif Ninjas <= 50:
+elif ninjas <= 50:
     print('''It'll be a struggle, but I can take em.''')
-elif Ninjas <= 30:
+elif ninjas <= 30:
     print('''I can fight those ninjas!''')
