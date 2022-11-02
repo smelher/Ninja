daab99762eaf9603336ff5bb0e258c614bb94b4f
smelher <79649846+smelher@users.noreply.github.com>
2022-11-02 11:49:32 -0400

    first commit

---
+++ ninjas_elif_statement.py
@@ -0,0 +1,8 @@
+
+Ninjas = 5
+if Ninjas <= 100:
+    print('''That's too many!''')
+elif Ninjas <= 50:
+    print('''It'll be a struggle, but I can take em.''')
+elif Ninjas <= 30:
+    print('''I can fight those ninjas!''')
